e8423870c6c68d7f010fbbffd603587daaa3416e
mucheol.Kim <ehdanfeorkfl@gmail.com>
2022-07-12 18:02:15 +0900

    애니메이션추가

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+// Loading Animation
+const loader = $('.loadingPage');
+const html = $('html');
+
+html.css({'overflow' : 'hidden'}); //로딩 중 스크롤 방지
+
+// $(window).on('load', ()=>{
+     setTimeout(() => { //  <-* 로딩속도 구현
+    loader.fadeOut(300);  
+    html.css({'overflow' : 'auto'}); //스크롤 방지 해제
+  // }, 2000);  //  <-* 로딩속도 구현
+})
+
 $(document).ready(function(){
+  // new fullpage('#fullpage', {
+    
+  // });
   $('#fullpage').fullpage({
     //options here
     anchors: ['section01', 'section02', 'section03', 'section04', 'section05', 'section06', 'section07'],
     menu: '#myMenu',
+    licenseKey: 'SK207-O5RWH-I32WH-1J2LI-UFKOO',
     autoScrolling:true,
     scrollHorizontally: true,
     dragAndMove : true
   });
 
-  
+  $('#content03').hover(function(){
   $('.subTextBox p').hover(function(){
     const img = document.querySelector('.backImg');
     $(this).siblings().removeClass('active');
@@ -16,34 +33,31 @@ $(document).ready(function(){
     if($(this).hasClass('sTBp01')){
       $('#content03 #inner').removeClass();
       $('#content03 #inner').addClass('p01');
-      // img.src = "/image/missionBack.png";
     }
     else if($(this).hasClass('sTBp02')){
       $('#content03 #inner').removeClass();
       $('#content03 #inner').addClass('p02');
-      // img.src = "/image/missionBack2.png";
     }
     else if($(this).hasClass('sTBp03')){
       $('#content03 #inner').removeClass();
       $('#content03 #inner').addClass('p03');
-      // img.src = "/image/missionBack3.png";
     }
     else if($(this).hasClass('sTBp04')){
       $('#content03 #inner').removeClass();
       $('#content03 #inner').addClass('p04');
-      // img.src = "/image/missionBack4.png";
     }
     else if($(this).hasClass('sTBp05')){
       $('#content03 #inner').removeClass();
       $('#content03 #inner').addClass('p05');
-      // img.src = "/image/missionBack5.png";
     }else {
-
+      $('#content03 #inner').removeClass();
+      
     }
-  // },function(){
-  //   $(this).removeClass('active');
-  //   img.src="";
   });
+},function(){
+  $('.subTextBox p').siblings().removeClass('active');
+  $('#content03 #inner').removeClass();
+});
 
 
   // slider
@@ -51,7 +65,6 @@ $(document).ready(function(){
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: false,
-    autoplaySpeed: 2000,
     responsive: [
       {
         breakpoint: 1366,
